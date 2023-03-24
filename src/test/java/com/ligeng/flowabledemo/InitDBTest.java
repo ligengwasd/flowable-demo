12ac65821ed24291293579a3f5680b2e3424cf96
geng.li <geng.li@timschina.com>
2023-03-24 19:19:17 +0800

    update

--- src/test/java/com/ligeng/flowabledemo/InitDBTest.java
+++ src/test/java/com/ligeng/flowabledemo/InitDBTest.java
@@ -1,6 +1,7 @@
 package com.ligeng.flowabledemo;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.junit.Test;
@@ -8,28 +9,18 @@ import org.junit.Test;
 import javax.sql.DataSource;
 
 
-public class InitDB {
-    private ProcessEngineConfiguration processEngineConfiguration;
-
-    public DataSource initDataSource() {
+public class InitDBTest {
+    public static void main(String[] args) {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/flowable?useUnicode=true&characterEncoding=UTF8&useSSL=false&serverTimezone=Asia/Shanghai&allowMultiQueries=true&allowPublicKeyRetrieval=true");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/flowable?useUnicode=true&characterEncoding=UTF8&useSSL=false&serverTimezone=Asia/Shanghai&allowMultiQueries=true&allowPublicKeyRetrieval=true&nullCatalogMeansCurrent=true");
         dataSource.setUsername("root");
         dataSource.setPassword("wasdwasd");
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setMaxActive(3);
         dataSource.setMinIdle(1);
-        return dataSource;
-    }
 
-
-    /**
-     * 初始化数据库表34张表
-     */
-    @Test
-    public void initDataBaseTables() {
-        processEngineConfiguration = new StandaloneProcessEngineConfiguration();
-        processEngineConfiguration.setDataSource(initDataSource());
+        ProcessEngineConfiguration processEngineConfiguration = new StandaloneProcessEngineConfiguration();
+        processEngineConfiguration.setDataSource(dataSource);
         processEngineConfiguration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
         processEngineConfiguration.buildProcessEngine();
     }
