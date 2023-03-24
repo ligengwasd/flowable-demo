7e22ff4cb3d70ff45d8e0dee72454611f4c5f458
geng.li <geng.li@timschina.com>
2023-03-24 18:57:42 +0800

    update

--- src/test/java/com/ligeng/flowabledemo/InitDB.java
+++ src/test/java/com/ligeng/flowabledemo/InitDB.java
@@ -3,7 +3,7 @@ package com.ligeng.flowabledemo;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
-import org.junit.Before;
+import org.junit.Test;
 
 import javax.sql.DataSource;
 
@@ -13,10 +13,10 @@ public class InitDB {
 
     public DataSource initDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/activiti?useUnicode=true&amp;characterEncoding=UTF8&amp;useSSL=false&amp;serverTimezone=Asia/Shanghai&amp;allowMultiQueries=true&amp;allowPublicKeyRetrieval=true");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/flowable?useUnicode=true&characterEncoding=UTF8&useSSL=false&serverTimezone=Asia/Shanghai&allowMultiQueries=true&allowPublicKeyRetrieval=true");
         dataSource.setUsername("root");
         dataSource.setPassword("wasdwasd");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setMaxActive(3);
         dataSource.setMinIdle(1);
         return dataSource;
@@ -26,7 +26,7 @@ public class InitDB {
     /**
      * 初始化数据库表34张表
      */
-    @Before
+    @Test
     public void initDataBaseTables() {
         processEngineConfiguration = new StandaloneProcessEngineConfiguration();
         processEngineConfiguration.setDataSource(initDataSource());
